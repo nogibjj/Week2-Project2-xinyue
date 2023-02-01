784745c70394ec0e8793265eef92eb8572866a85
xyli1233 <123574025+xyli1233@users.noreply.github.com>
2023-02-01 19:19:26 +0000

    project1

--- rust_project/src/lib.rs
+++ rust_project/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn marco_polo(input: &str) -> String {
     if input == "Marco" {
-        "Polo!".to_string()
+        "Welcome, polo!".to_string()
     } else {
-        "Not Marco".to_string()
+        "Denied, not Marco".to_string()
     }
 }
